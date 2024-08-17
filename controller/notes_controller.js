22e8672aebf667ab45a3c9ad59be6e2b7fcf5127
asadrafi <asadrafi1221@gmail.com>
2024-08-18 01:04:24 +0500

    added tags functionality

--- controller/notes_controller.js
+++ controller/notes_controller.js
@@ -1,13 +1,11 @@
 import db_data from "../../todos_proj/db_connection/db_connection.js";
 
-
 const display_notes = (req, res) => {
     const email = req.query.email;
     res.render('notes', { email: email });
 }
 const get_data = async (req, res) => {
     const data = (await db_data.query('select * from clients_data join users_notes on clients_data.email = users_notes.email')).rows;
-
     res.send(data);
 }
 const delete_post = async (req, res) => {
@@ -26,34 +24,68 @@ const delete_post = async (req, res) => {
 };
 
 
-const submit_note = async (req, res) => {
-    const { new_post, email } = req.body;
-    const queryText = `
-            INSERT INTO users_notes (email, post) 
-            VALUES ($1, $2) 
-        `;
-    const queryValues = [email, new_post];
 
-    await db_data.query(queryText, queryValues);
+
+
+const submit_note = async (req, res) => {
+    const { new_post, email, tag } = req.body;
+    if (new_post&& email &&tag!==null) {
+        console.log('this is data : ');
+        console.log(new_post, email, tag);
+
+        try {
+            const queryText = `
+            INSERT INTO users_notes (email, post ,tags) 
+            VALUES ($1, $2 ,$3) 
+        `;
+            const queryValues = [email, new_post, tag];
+
+            await db_data.query(queryText, queryValues);
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+    }
+    else {
+        try {
+            console.log(new_post, email, tag);
+            const queryText = `
+                INSERT INTO users_notes (email, post ) 
+                VALUES ($1, $2) 
+            `;
+            const queryValues = [email, new_post];
+
+            await db_data.query(queryText, queryValues);
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+    }
     res.send('done');
 }
 
 const edit_post = async (req, res) => {
     const { post, newpost, email } = req.body;
-console.log('this is data  : ')
-    console.log(post , newpost , email);
+
+    console.log('this is data  : ')
+    console.log(post, newpost, email);
     const queryText = `
     UPDATE users_notes
     SET post = $1
     WHERE email = $2
-      AND post = $3
-`;
-const queryValues = [newpost, email, post];
+      AND post = $3`;
+    const queryValues = [newpost, email, post];
 
-const result = await db_data.query(queryText, queryValues);
+    const result = await db_data.query(queryText, queryValues);
 
     res.send('done');
 }
+
+
+
+
 
 
 export { display_notes, get_data, delete_post, submit_note, edit_post };
